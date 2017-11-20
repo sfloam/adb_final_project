b49fa641800e23f2b6730aea0b42cdd9957bb67c
sfloam <scottfloam@gmail.com>
2017-11-19 21:28:35 -0500

    converted LockTable into concurrent hashmap. Not finished. Just setting up templates here" ; "

--- LockTable.java
+++ LockTable.java
@@ -3,8 +3,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 
 public class LockTable extends ConcurrentHashMap<String, String>{
-	
-		
+	int id;
 
+	LockTable (int id){
+		this.id = id;
+	}
+	
+	public String toString(){
+		return "LT_" + id;
+	}
 
 }
