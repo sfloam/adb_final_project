5eaa52ae39df986a8d5e43646ce1cb80eccffac0
Pratik Parag Karnik <ppk238@nyu.edu>
2017-12-02 00:34:50 -0500

    Locktable modifications

--- adb_final/adb_final_proj/src/GlobalConstants.java
+++ adb_final/adb_final_proj/src/GlobalConstants.java
@@ -3,5 +3,7 @@ public class GlobalConstants {
 
 	public final static int sites = 10;
 	public final static int variables = 20;
+	public final static String readLock = "RL";
+	public final static String writeLock = "WL";
 	
 }
